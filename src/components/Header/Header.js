e3cff04de207f18ce187e552759463a057172e03
karl wernsman <wernsmank@gmail.com>
2023-01-24 11:43:21 -0800

    three colors hard coded in header as NavLinks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <NavLink to="/rgb/42/157/143">Persian Green</NavLink>
+      <NavLink to="/rgb/231/111/81">Burnt Sienna</NavLink>
+      <NavLink to="/rgb/233/196/106">Maize Crayola</NavLink>
+    </header>
+  );
 }
