86106d490946858a4c40ba7766da07e3abc1c702
karl wernsman <wernsmank@gmail.com>
2023-01-24 12:18:57 -0800

    5 colors hard coded in header and css updates to page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <NavLink to="/rgb/42/157/143">Persian Green</NavLink>
-      <NavLink to="/rgb/231/111/81">Burnt Sienna</NavLink>
-      <NavLink to="/rgb/233/196/106">Maize Crayola</NavLink>
+      <NavLink to="/rgb/38/70/83" className="charcoal">
+        Charcoal
+      </NavLink>
+      <NavLink to="/rgb/42/157/143" className="green">
+        Persian Green
+      </NavLink>
+      <NavLink to="/rgb/233/196/106" className="crayola">
+        Maize Crayola
+      </NavLink>
+      <NavLink to="/rgb/244/162/97" className="brown">
+        Sandy Brown
+      </NavLink>
+      <NavLink to="/rgb/231/111/81" className="sienna">
+        Burnt Sienna
+      </NavLink>
     </header>
   );
 }
