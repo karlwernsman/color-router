6a5f87f0aa349ae431f379ebb941e4e313d5c67f
karl wernsman <wernsmank@gmail.com>
2023-01-24 11:22:23 -0800

    h1 element in Colors displaying actual rbg values

--- src/components/Colors/Colors.js
+++ src/components/Colors/Colors.js
@@ -5,7 +5,9 @@ export default function Colors() {
   let { r, g, b } = useParams();
   return (
     <div style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <h1>rgb</h1>
+      <h1>
+        rgb({r}, {g}, {b})
+      </h1>
     </div>
   );
 }
